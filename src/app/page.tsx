af9d9c118c602aff726b679faef8e0a4514fa5c5
Hoac Phong <hphong@allivet.com>
2024-03-28 10:31:02 +0700

    Deploy portfolio app

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import Header from "./components/Header";
+
+export default function Home() {
+    return (
+        <div>
+            {/* Header */}
+            <Header/>
+            {/* Hero */}
+            {/* Experience */}
+            {/* Skill */}
+            {/* Project */}
+            {/* Contact */}
+        </div>
+    );
+}
